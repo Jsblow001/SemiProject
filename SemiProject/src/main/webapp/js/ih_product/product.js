2158ed18de57ef526c30ed28576e74ea70d140cd
user <user@DESKTOP-MRA1F63>
2026-01-05 17:17:39 +0900

    goWish() 함수 수정 - 찜하기 삭제시, location.reload(true); reload 되도록 설정

--- SemiProject/src/main/webapp/js/ih_product/product.js
+++ SemiProject/src/main/webapp/js/ih_product/product.js
@@ -22,7 +22,8 @@ function goWish(productId, contextPath) {
             else if(json.result === "removed") {
                 alert(json.message);
                 $wishIcon.removeClass("fas text-danger").addClass("far");
-            }
+				location.reload(true);
+			}
 			// far는 비어있는 하트 
 			// fas는 색이 꽉 찬 하트
         },
